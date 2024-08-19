75e2b6ca9d275e0de9d7b4e5073d93eae1467cae
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-08-19 15:16:44 +0300

    fix

--- server/app/src/main/java/org/example/app/model/entity/BaseEntity.java
+++ server/app/src/main/java/org/example/app/model/entity/BaseEntity.java
@@ -3,8 +3,10 @@ package org.example.app.model.entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UuidGenerator;
 
+import java.sql.Types;
 import java.util.UUID;
 
 @Data
@@ -12,5 +14,6 @@ import java.util.UUID;
 public abstract class BaseEntity {
     @Id
     @UuidGenerator
+    @JdbcTypeCode(Types.VARCHAR)
     private UUID id;
 }
