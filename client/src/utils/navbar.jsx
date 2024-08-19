184c9129ea6504076a1d9e4beeb283d364e8fc83
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-08-19 18:11:16 +0300

    update

--- client/src/utils/navbar.jsx
+++ client/src/utils/navbar.jsx
@@ -1,5 +1,20 @@
+import "../styles/navbar.css"
+import { Link } from "react-router-dom"
+
 export default function Navbar() {
     return (
-        <>Navbar</>
+        <>
+            <nav className="navigation" id="navigation">
+                <li className="user">
+                    <p id="userMsg">Welcome, <span>Name.</span></p>
+                </li>
+                <li className="user"><Link id="createLink" className="linkBtn" href="/create">Create</Link></li>
+                <li><Link id="homeLink" className="linkBtn" href="/home">Home</Link></li>
+                <li><Link id="searchLink" className="linkBtn" href="/search">Search</Link></li>
+                <li className="user right guest"><Link id="logoutLink" className="linkBtn" href="/logout">Logout</Link></li>
+                <li className="right guest"><Link id="loginLink" className="linkBtn" href="/login">Login</Link></li>
+                <li className="right guest"><Link id="registerLink" className="linkBtn" href="/register">Register</Link></li>
+            </nav>
+        </>
     );
 }
