c5a6f3ed05957bc5e4377dee13108fc15897ab19
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-08-20 13:02:03 +0300

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,9 +3,10 @@ import Home from "./components/homePage";
 import Navbar from "./utils/navbar";
 import Footer from "./utils/footer";
 import Login from "./components/loginPage";
+import {useState} from "react";
 
 function App() {
-
+    const [auth, setAuth] = useState(false);
     return (
         <>
             <Navbar />
