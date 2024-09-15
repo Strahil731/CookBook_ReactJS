7c9736bd7438ffe03daf0324f5211a1a9902bbc3
kalin73 <104739268+kalin73@users.noreply.github.com>
2024-09-15 13:50:45 +0300

    fix

--- server/app/src/main/java/org/example/app/model/entity/IngredientEntity.java
+++ server/app/src/main/java/org/example/app/model/entity/IngredientEntity.java
@@ -1,21 +1,25 @@
 package org.example.app.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 @Entity
 @Table(name = "ingredients")
+@NoArgsConstructor
+@AllArgsConstructor
 public class IngredientEntity extends BaseEntity {
-    @ManyToOne(targetEntity = RecipeEntity.class)
-    @JoinColumn(name = "recipe_id")
-    private RecipeEntity recipeId;
-
     @Column
     private String name;
 
     @Column
     private String quantity;
+
+    @ManyToOne(targetEntity = RecipeEntity.class)
+    @JoinColumn(name = "recipe_id")
+    private RecipeEntity recipeId;
 }
